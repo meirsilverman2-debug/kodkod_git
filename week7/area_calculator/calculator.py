c047172fec2074244f5c6ea4e29f9fef4acc8c3e
Meir925man <meirsilverman2@gmail.com>
2026-05-31 02:02:41 +0300

    added the firs file in the shape project which inside it is the shape class

---
+++ week7/area_calculator/calculator.py
@@ -0,0 +1,18 @@
+# # Level-1_Shape:
+
+class Shape:
+    def __init__(self, shape_id, shape_type):
+        self.id = shape_id
+        self.shape_type = shape_type
+
+    def get_area(self):
+        pass
+
+    def get_perimter(self):
+        pass
+
+    def to_dict(self):
+        pass
+
+    def __str__(self):
+        pass
