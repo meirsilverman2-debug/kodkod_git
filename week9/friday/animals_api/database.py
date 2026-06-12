b7845e7e807c0c8520d12a917c36ad8bdc0000fc
Meir925man <meirsilverman2@gmail.com>
2026-06-12 13:51:17 +0300

    adding the connection function that hold the config to which database we are connecting to

---
+++ week9/friday/animals_api/database.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="1234",
+        port=3306,
+        database="animals_db"
+
+    )
